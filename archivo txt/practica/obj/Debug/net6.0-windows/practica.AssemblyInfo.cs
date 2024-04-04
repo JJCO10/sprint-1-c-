9a855043a16b6b98760a91c91b5f5b50cde48b09
JJCO10 <juanjosecarmonaortiz@gmail.com>
2024-04-04 12:16:01 -0500

    actualizacion

--- archivo txt/practica/obj/Debug/net6.0-windows/practica.AssemblyInfo.cs
+++ archivo txt/practica/obj/Debug/net6.0-windows/practica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("practica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2532fb93faabaa2bd712be7345aabd48c42d6f7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+981a4ed10d6e209081bde628fb82e01d806f0ef2")]
 [assembly: System.Reflection.AssemblyProductAttribute("practica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("practica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
